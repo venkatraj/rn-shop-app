21ffe2fb5edf8929c1d2484266ccf23271e58900
Venkat <venkat@codinggeek.com>
2020-06-04 16:27:19 +0530

    styling produc details

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,17 +1,50 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  ScrollView,
+  View,
+  Button,
+  Text,
+  StyleSheet,
+  Image,
+} from 'react-native';
+import Colors from '../../constants/Colors';
+// import { ScrollView } from 'react-native-gesture-handler';
 
 const ProductDetailScreen = (props) => {
-  const { route, navigation } = props;
-  const { title } = route.params.product;
+  const { route } = props;
+  const { imageUrl, price, description } = route.params.product;
 
   return (
-    <View>
-      <Text>{title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: imageUrl }} />
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title="Add To Cart" />
+      </View>
+      <Text style={styles.price}>{price}</Text>
+      <Text style={styles.description}>{description}</Text>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 300,
+    width: '100%',
+  },
+  actions: {
+    marginVertical: 20,
+    alignItems: 'center',
+  },
+  price: {
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'center',
+    margin: 20,
+  },
+});
 
 export default ProductDetailScreen;
